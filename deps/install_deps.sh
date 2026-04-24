c9d2d1da8c91f920c719fed028b241e0fc77d3b3
Daniel Waddington <daniel.waddington@ibm.com>
2026-04-23 17:41:28 -0700

    fixed clang-devel

--- deps/install_deps.sh
+++ deps/install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo dnf install -y fuse3-devel fuse3-libs numactl-libs numactl numactl-devel ninja-build CUnit-devel libuuid-devel libaio-devel ncurses-devel openssl-devel 
-sudo dnf install -y clang libclang-devel glibc-headers glibc-devel gcc gcc-c++ make pkgconfig
+sudo dnf install -y fuse3-devel fuse3-libs numactl-libs numactl numactl-devel ninja-build CUnit-devel libuuid-devel libaio-devel ncurses-devel openssl-devel
+sudo dnf install -y clang clang-devel glibc-headers glibc-devel gcc gcc-c++ make pkgconfig
